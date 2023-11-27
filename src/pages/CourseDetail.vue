d2f472eea67bf759f7764cb91a122fda0caa29c1
FirstFirstFirstFirst <hocrux1st.first@gmail.com>
2023-11-28 02:57:29 +0700

    linked 2 pages

--- src/pages/CourseDetail.vue
+++ src/pages/CourseDetail.vue
@@ -1,25 +1,28 @@
 <template>
-  <div v-if="course">
-    <h2>{{ course.title }}</h2>
-    <p>{{ course.description }}</p>
-    <p>Syllabus: {{ course.syllabus }}</p>
-    <p>Price: {{ course.pricing }}</p>
+  <div class="course-detail-page">
+    <div v-if="course">
+      <h2 class="text-h3 q-mb-md">{{ course.title }}</h2>
+      <p class="text-body1 q-mb-md">{{ course.description }}</p>
+      <p class="text-subtitle2 q-mb-md">Syllabus: {{ course.syllabus }}</p>
+      <p class="text-subtitle2 q-mb-md">Price: ${{ course.pricing }}</p>
 
-    <h3>Lesson Plans</h3>
-    <ul>
-      <li v-for="lessonPlan in course.lessonPlans" :key="lessonPlan.id">{{ lessonPlan.title }}</li>
-    </ul>
+      <h3 class="text-h5 q-mb-md">Lesson Plans</h3>
+      <ul class="q-pl-md">
+        <li v-for="lessonPlan in course.lessonPlans" :key="lessonPlan.id" class="text-body2 q-mb-xs">{{ lessonPlan.title }}</li>
+      </ul>
 
-    <h3>Sample Lessons</h3>
-    <ul>
-      <li v-for="lesson in course.sampleLessons" :key="lesson.id">{{ lesson.title }}</li>
-    </ul>
+      <h3 class="text-h5 q-mb-md">Sample Lessons</h3>
+      <ul class="q-pl-md">
+        <li v-for="lesson in course.sampleLessons" :key="lesson.id" class="text-body2 q-mb-xs">{{ lesson.title }}</li>
+      </ul>
 
-    <!-- Link to navigate back to the course list -->
-    <router-link :to="{ name: 'course-list' }">Back to Course List</router-link>
-  </div>
-  <div v-else>
-    <p>Loading...</p>
+      <!-- Link to navigate back to the course list -->
+      <router-link to="/courses" class="text-subtitle1 q-mt-md">Back to Course List</router-link>
+    </div>
+    <div v-else>
+      <q-spinner-square size="80px" color="primary" />
+      <p class="text-h6 q-mt-md">Loading...</p>
+    </div>
   </div>
 </template>
 
@@ -33,11 +36,9 @@ export default {
     };
   },
   mounted() {
-    // Fetch course details based on courseId from the route parameters
     const courseId = this.$route.params.id;
 
-    // Fetch course details from your API
-    axios.get(`/api/courses/${courseId}`)
+    axios.get(`http://localhost:3000/api/courses/${courseId}`)
       .then(response => {
         this.course = response.data;
       })
@@ -49,5 +50,4 @@ export default {
 </script>
 
 <style scoped>
-/* Add your styles here */
 </style>
--- src/pages/CourseList.vue
+++ src/pages/CourseList.vue
@@ -1,30 +1,44 @@
 <template>
   <q-page class="course-list-page q-pa-md">
     <div v-if="courses.length > 0">
-      <h2 class="q-mb-md text-h6">Course Listing</h2>
-      <q-list bordered>
-        <q-item v-for="course in courses" :key="course.course_id">
+      <h2 class="mb-8 text-lg font-semibold">Course Listing</h2>
+      <q-list bordered class="mb-8">
+        <q-item
+          v-for="course in courses"
+          :key="course.course_id"
+          class="p-4 course-item transition duration-300 ease-in-out transform hover:shadow-md"
+        >
           <q-item-section>
             <q-item-label>
-              <h4 class="text-h6 q-mb-sm">{{ course.title }}</h4>
-              <p class="q-mb-md">{{ course.description }}</p>
+              <h4 class="text-lg mb-4">{{ course.title }}</h4>
+              <p class="mb-4">{{ course.description }}</p>
             </q-item-label>
           </q-item-section>
           <q-item-section side top>
-            <q-item-label>
-              <div class="q-mb-xs">
+            <q-item-label class="flex flex-col">
+              <div class="mb-2">
                 <strong>Language:</strong> {{ course.language }}
               </div>
-              <div class="q-mb-xs">
+              <div class="mb-2">
                 <strong>Proficiency Level:</strong>
                 {{ course.proficiency_level }}
               </div>
-              <div class="q-mb-xs">
+              <div class="mb-2">
                 <strong>Instructor:</strong> {{ course.instructor_name }}
               </div>
               <div class="text-primary">
                 <strong>Pricing:</strong> ${{ course.pricing }}
               </div>
+              <!-- Add View Details link -->
+              <router-link
+                class="text-accent mt-4"
+                :to="{
+                  name: 'course-detail',
+                  params: { id: course.course_id },
+                }"
+              >
+                View Details
+              </router-link>
             </q-item-label>
           </q-item-section>
         </q-item>
@@ -32,7 +46,7 @@
     </div>
     <div v-else>
       <q-spinner-square size="80px" color="primary" />
-      <div class="q-mt-md text-h6">No courses available</div>
+      <div class="mt-4 text-lg">No courses available</div>
     </div>
   </q-page>
 </template>
@@ -49,10 +63,7 @@ export default {
   methods: {
     async fetchCourses() {
       try {
-        // const response = await axios.get("http://localhost:3000/api/courses");
-        const response = await axios.get(
-          "https://language-learning-backend-rosy.vercel.app/api/courses"
-        );
+        const response = await axios.get("http://localhost:3000/api/courses");
         this.courses = response.data;
       } catch (error) {
         console.error("Error fetching courses", error);
